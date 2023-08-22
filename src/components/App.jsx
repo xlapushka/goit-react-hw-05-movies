4e2f8824ba57fb42f5b9ebdac3e5df0fcd83dc31
Yuliia Hubarenko <xlapushka@ukr.net>
2023-08-22 13:44:51 +0300

    second commit (перевірка)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
+import { Route, Routes, NavLink } from 'react-router-dom';
+
+import css from './styles.module.css';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <header className={css.header}>
+        <ul className={css.headerNav}>
+          <li>
+            <NavLink to="/" className={css.link}>
+              <button type="button" className={css.headerNavBtn}>
+                Home
+              </button>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies" className={css.link}>
+              <button type="button" className={css.headerNavBtn}>
+                Movies
+              </button>
+            </NavLink>
+          </li>
+        </ul>
+      </header>
+
+      <section className={css.section}>
+        <Routes>
+          <Route path="/" element={<div>HomePage</div>} />
+          <Route path="/movies" element={<div>Movies</div>} />
+        </Routes>
+      </section>
     </div>
   );
 };
